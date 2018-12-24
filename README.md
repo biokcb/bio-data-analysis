b56a0d96e29702fea9ce3052d5e7b5984195173b
kristen <kristen.chong.brown@gmail.com>
2018-12-24 16:01:23 -0500

    reorganization of files
    moving files around to make it more generalized for random, shorter project consisting of biological data analysis scripts. Also combined old trim/tail script into one.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Scripts for biological data analysis
 Scripts I've written for analyzing various types of data that were not large enough projects to warrant their own repository. Mainly for analyzing and plotting next generation sequencing data, but other random scripts may get tossed in here from time to time. 
+
+# Folders
+This is organized into folders based on broader categories for the type of data analysis or if it was a short, individual project. Here is the basic breakdown:
+
+
+- lab_assays: Data analysis scripts for various wet lab experiments I did
+- next_gen_sequencing: Data analysis scripts & short projects for various NGS data sets, usually mRNA-seq and smRNA-seq. Some for taking raw sequencing data to counts tables and further exploratory analysis and plotting.
+- microarray: Data analysis of microarray datasets
+- imaging: Imaging data analysis
+- misc: everything else
--- scripts/next_gen_sequencing/trim_tail.py
+++ scripts/next_gen_sequencing/trim_tail.py
@@ -10,8 +10,82 @@
 ###############################################
 import sys
 import os.path
-from lib.cmd_utils import run_cmd
-from lib.file_util import grabLine, addLine, headerRep
+import subprocess
+
+def run_cmd(cmd):  
+	""" 
+	Runs a command line tool/command entered. 
+	Returns stdout, stderr 
+	
+	cmd = command to run
+	"""
+	print('Now running:')
+	print(cmd)
+	process = subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE)
+	output, error = process.communicate()
+	
+  return output, error
+	
+def grouped(iterator, size):
+	""" 
+	return tuple of a certain size
+	
+	"""
+	yield tuple(next(iterator) for _ in range(size))
+	
+def grabLine(file, num):
+	""" 
+	return a list containing num lines from
+	a file. 
+	
+	file = file to grab lines from
+	num = number of lines to grab
+	"""
+	fileList = []
+	append = fileList.append
+	for i in num:
+		line = str(file[i]).strip('\n')
+		append(line.strip('\n'))
+	return(fileList)
+	
+def addLine(finalList, filename):
+	"""
+	Add lines to a file
+	
+	finalList = list of lines to add
+	filename = file to add to
+	"""
+	with open(filename, "a") as f:
+  	 	f.writelines(finalList)
+  	 	
+def headerRep(fastqFile, outFile):
+	""" 
+	Replace header of a fastq file with a simpler
+	name for compatibility with some tools
+	
+	@seq1 .... @seqN
+	
+	fastqFile = input fastq file to clean
+	outFile = output file name to write
+  """
+  # get number of lines first
+    with open(fastqFile) as f:
+        size=sum(1 for _ in f)
+    f.close()      
+  
+  # replace header
+  with open(fastqFile) as f:
+      fastq = f.readlines()
+      lineNum = (0,1,2,3)
+      countSeq=0
+      
+      while lineNum[0] < size:
+          seqHeader, seq, extraName, seqQuality = grabLine(fastq, lineNum)
+          countSeq += 1     
+          finalList = ['@seq'+str(countSeq)+'\n', seq+'\n', extraName+'\n', seqQuality+'\n']
+          addLine(finalList, outFile) 
+          lineNum = (lineNum[0] + 4, lineNum[1] + 4, lineNum[2] + 4, lineNum[3] + 4)      
+      f.close() 
 
 # write accepted hits table
 def appendTrimSuccess(outFile, loopNum, tophatFolder):
@@ -146,4 +220,3 @@ if __name__ == '__main__':
     outFile = sys.argv[2]
     tophatFolder = sys.argv[3]
     main(fastqFileIN, outFile, tophatFolder) 
-
